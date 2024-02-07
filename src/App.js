33ad3bd9753f9b4da3e8ff1972e0fbe7a6427259
Gourav Gupta <gouravgupta7799@gmail.com>
2024-02-07 17:47:14 +0530

    get data from backend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './components/Store/uiActions';
+import { sendCartData, fetchCartData } from './components/Store/Cart-Actions';
 let isInitial = true;
 
 function App() {
@@ -15,53 +15,16 @@ function App() {
   console.log(notification)
   const dispatch = useDispatch();
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending",
-          message: "Sending cart data!",
-        })
-      );
+    dispatch(fetchCartData());
+  }, [dispatch])
 
-      const response = await fetch(
-        "https://expensetracker-ce323-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("sending cart data failed");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully",
-        })
-      );
-    };
-
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed",
-        })
-      );
-    });
-
-
+    dispatch(sendCartData(cart))
   }, [cart, dispatch]);
-
   return (
     <React.Fragment>
       {notification && (
